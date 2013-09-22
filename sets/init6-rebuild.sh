032e280bc483986aad0a6e01d10051b1104f8f07
Andrey Ovcharov (init_6) <sudormrfhalt@gmail.com>
2013-09-22 16:07:52 +0300

    Fix sets/init6-rebuild.sh

--- sets/init6-rebuild.sh
+++ sets/init6-rebuild.sh
@@ -2,6 +2,6 @@ ROOT=$(portageq envvar ROOT);
 REPO_ROOT=$(portageq get_repo_path "${ROOT}" init6)
 installed=$(portageq match "${ROOT}" "");
 
-for ebuild in $(find "${REPO_ROOT}" -name "*-9999.ebuild" | sed -r "s|^.*/([[:alnum:]_\.\-]+)/[^\/]+/([[:alnum:]_\.\-]+)\.ebuild|\1/\2|" ); do
+for ebuild in $(find "${REPO_ROOT}" -name "*.ebuild" | sed -r "s|^.*/([[:alnum:]_\.\-]+)/[^\/]+/([[:alnum:]_\.\-]+)\.ebuild|\1/\2|" ); do
 	echo "${installed}" | grep --color=never "${ebuild}" | sed 's/^/=/';
 done
